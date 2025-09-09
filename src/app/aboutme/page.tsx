baf92f498714a0c89d4025b410a1fcfb0bb410fe
hamimsardardev <hamimsardar118677@gmail.com>
2025-09-09 18:41:42 +0600

    routing setup

---
+++ src/app/aboutme/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Aboutme = () => {
+  return (
+    <div>
+      this is about page
+    </div>
+  )
+}
+
+export default Aboutme
---
+++ src/app/contactme/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Contactme = () => {
+  return (
+    <div >
+      <h1 className="text-primary">this is contact page</h1>
+    </div>
+  )
+}
+
+export default Contactme
