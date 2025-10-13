6dcd769d00e9b7d7fcb580e112dd813de6360f2d
hamimsardardev <hamimsardar118677@gmail.com>
2025-10-13 17:56:20 +0600

    about animation add

--- src/app/components/aboutPart.tsx
+++ src/app/components/aboutPart.tsx
@@ -55,40 +55,46 @@ const AboutPart = () => {
               </motion.div>
             </div>
             <div>
-              <div>
-                <h3 className="font-roboto text-[15px] font-bold leading-[22px] text-third">
-                  About
-                </h3>
-                <h2 className="font-dmSans text-[30px] font-normal leading-[35px] mt-3 text-primary mb-4">
-                  Who am i
-                </h2>
-                <p className="font-roboto text-[16px] font-normal leading-[24px] text-secondery">
-                  I've created and updated websites for many different clients.
-                  I worked with multiple CMS including WordPress, Joomla, and
-                  Drupal, as well as created my own custom website management
-                  system in Node.js
-                </p>
-                <h2 className="font-dmSans text-[30px] font-normal leading-[35px] mt-3 text-primary mb-4">
-                  I Can Develop Anything You Want
-                </h2>
-                <p className="font-roboto text-[16px] font-normal leading-[24px] text-secondery">
-                  A part of the team that has built the front-end for an
-                  enterprise-level web application in AngularJS. I've been a
-                  part of a team that has built.
-                </p>
-                <h2 className="font-dmSans text-[30px] font-normal leading-[35px] mt-3 text-primary mb-4">
-                  I can follow digital Trends
-                </h2>
-                <p className="font-roboto text-[16px] font-normal leading-[24px] text-secondery">
-                  I've worked with a wide range of technologies, including MS
-                  SQL Server and Python. One of my favorite characteristics of
-                  an employer is their passion for the work they are doing.
-                </p>
-                <div className="flex gap-4 mt-8">
-                  <div className="button">My Project</div>
-                  <div className="button">Download CV</div>
+              <motion.div
+                initial={{ opacity: 0, scale: 0.5 }}
+                whileInView={{ opacity: 1, scale: 1 }}
+                transition={{ duration: 0.5 }}
+              >
+                <div>
+                  <h3 className="font-roboto text-[15px] font-bold leading-[22px] text-third">
+                    About
+                  </h3>
+                  <h2 className="font-dmSans text-[30px] font-normal leading-[35px] mt-3 text-primary mb-4">
+                    Who am i
+                  </h2>
+                  <p className="font-roboto text-[16px] font-normal leading-[24px] text-secondery">
+                    I've created and updated websites for many different
+                    clients. I worked with multiple CMS including WordPress,
+                    Joomla, and Drupal, as well as created my own custom website
+                    management system in Node.js
+                  </p>
+                  <h2 className="font-dmSans text-[30px] font-normal leading-[35px] mt-3 text-primary mb-4">
+                    I Can Develop Anything You Want
+                  </h2>
+                  <p className="font-roboto text-[16px] font-normal leading-[24px] text-secondery">
+                    A part of the team that has built the front-end for an
+                    enterprise-level web application in AngularJS. I've been a
+                    part of a team that has built.
+                  </p>
+                  <h2 className="font-dmSans text-[30px] font-normal leading-[35px] mt-3 text-primary mb-4">
+                    I can follow digital Trends
+                  </h2>
+                  <p className="font-roboto text-[16px] font-normal leading-[24px] text-secondery">
+                    I've worked with a wide range of technologies, including MS
+                    SQL Server and Python. One of my favorite characteristics of
+                    an employer is their passion for the work they are doing.
+                  </p>
+                  <div className="flex gap-4 mt-8">
+                    <div className="button">My Project</div>
+                    <div className="button">Download CV</div>
+                  </div>
                 </div>
-              </div>
+              </motion.div>
             </div>
           </div>
         </div>
